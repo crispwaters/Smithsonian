cef0041cf46e3793be440f3d716a1c04893fa83f
Michael Ore <selbram1993@gmail.com>
2012-06-09 13:57:49 -0500

    Queue interval is now 20 seconds

--- server/queueserver.erl
+++ server/queueserver.erl
@@ -39,7 +39,7 @@ handle_cast({post_message, Message}, Queue) ->
     Empty = queue:is_empty(Queue),
     if
 	Empty ->
-	    {ok, TRef} = timer:apply_interval(10000, queueserver, dequeue, []),
+	    {ok, TRef} = timer:apply_interval(20000, queueserver, dequeue, []),
 	    put(timer, TRef);
 	true ->
 	    ok
