7c09d9a6c7af39215af88461d35f220fcb98c9f5
Qian Zhang <qianz424@gmail.com>
2012-06-09 13:25:18 -0500

    Added test folder

---
+++ test/js/functions.js
@@ -0,0 +1,36 @@
+var cur_div = '#page1';
+var div_stack = new Array;
+init();
+
+$('.start').click(function() {
+	start_click();
+});
+
+$('.next').click(function() {
+	next_click();
+});
+
+$('.back').click(function() {
+	back_click();
+});
+
+function init(){
+	cur_div = '#page1';
+	$(cur_div).fadeIn(500);
+}
+
+function start_click(){
+	init();
+}
+
+function next_click(){
+	div_stack.push(cur_div);
+	
+}
+
+function back_click(){
+	cur_div = div_stack.pop();
+	$(cur_div).fadeIn(500);
+
+}
+
